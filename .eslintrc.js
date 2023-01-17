7194c2b4bd2cc4d94787a770544405eae5598227
nicholidev <nicholijin@outlook.com>
2023-01-18 03:10:44 +0800

    update test

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     ],
     parser: "@typescript-eslint/parser",
     parserOptions: {
-        project: ["tsconfig.json", "tsconfig.dev.json"],
+        project: ["tsconfig.json", "tsconfig-cjs.json"],
         tsconfigRootDir: __dirname,
         sourceType: "module",
     },
